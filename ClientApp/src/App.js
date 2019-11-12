5701494b30aa88e8d2af9873314eac8c0f01540d
Swampen <ole-ma@outlook.com>
2019-11-12 12:09:28 +0100

    Added active on link in navbar

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-
+import { ContactUs } from './components/ContactUs';
 
 import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
-
   render () {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
+        <Route exact path='/contactus' component={ContactUs} />
       </Layout>
     );
   }
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -4,40 +4,62 @@ import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+    static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true,
+            active: ''
+        };
+    }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
 
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Vy FAQ</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+    linkClickedHandler(route) {
+        this.setState({ active: route })
+    }
+
+    componentDidMount() {
+        this.setState({ active: window.location.pathname.toLocaleLowerCase() })
+    }
+
+    render() {
+        return (
+            <header>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                    <Container>
+                        <NavbarBrand tag={Link} to="/">Vy FAQ</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                        <Collapse className="d-sm-inline-flex flex-sm-row" isOpen={!this.state.collapsed} navbar>
+                            <ul className="navbar-nav flex-grow">
+                                <NavItem>
+                                    <NavLink
+                                        tag={Link}
+                                        className={this.state.active === "/" ? "active" : "text-dark"}
+                                        onClick={() => this.linkClickedHandler("/")}
+                                        to="/">Home
+                                        </NavLink>
+                                </NavItem>
+
+                                <NavItem>
+                                    <NavLink tag={Link}
+                                        className={this.state.active === "/contactus" ? "active" : "text-dark"}
+                                        onClick={() => this.linkClickedHandler("/contactus")}
+                                        to="/ContactUs">Contact Us
+                                        </NavLink>
+                                </NavItem>
+                            </ul>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+        );
+    }
 }
